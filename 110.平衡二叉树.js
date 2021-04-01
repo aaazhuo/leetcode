6311d3c2b61b6b94ec11d98305b57433f8ca99ba
骏壹 <renzhuo.rz@alibaba-inc.com>
2021-04-01 10:10:28 +0800

    feat: 平衡二叉树

--- 110.平衡二叉树.js
+++ 110.平衡二叉树.js
@@ -18,17 +18,29 @@
  * @return {boolean}
  */
 var isBalanced = function(root) {
-  if (!root) return true;
-  function dfs (root, depth) {
+  // 自顶向下遍历
+  // if (!root) return true;
+  // function dfs (root, depth) {
+  //   if (!root) return 0;
+  //   const left = dfs (root.left, depth + 1);
+  //   const right = dfs (root.right, depth + 1);
+  //   return Math.max(left, right) + 1;
+  // }
+  // if (Math.abs(dfs(root.left, 0) - dfs(root.right, 0)) > 1) {
+  //   return false;
+  // }
+  // return isBalanced(root.left) && isBalanced(root.right)
+  // 自底向上遍历
+  function epilogue(root) {
     if (!root) return 0;
-    const left = dfs (root.left, depth + 1);
-    const right = dfs (root.right, depth + 1);
+    const left = epilogue(root.left);
+    const right = epilogue(root.right);
+    if (left === -1 || right === -1 || Math.abs(left - right) > 1) {
+      return -1;
+    }
     return Math.max(left, right) + 1;
   }
-  if (Math.abs(dfs(root.left, 0) - dfs(root.right, 0)) > 1) {
-    return false;
-  }
-  return isBalanced(root.left) && isBalanced(root.right)
+  return epilogue(root) >= 0;
 };
 // @lc code=end
 
